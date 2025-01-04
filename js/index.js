08a654d037b6521830965c556c1242ac8e28fc7f
Максим Савчук <friiiman94@gmail.com>
2025-01-04 17:48:57 +0300

    js async func

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
-fetch("js/data.json")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        data = data.reverse().slice(0, 8);
-        
-        const cardsContainer = document.querySelector(".products__main");
-        data.forEach(({ productId, imgSrc, productName, productPrice }) => {
-            const productCardEl = `
-                <a id="${productId}" href="#" class="product-card">
-                    <img src="${imgSrc}" alt="Product photo" class="product-card__img">
-                    <div class="product-card__text">
-                        <span class="product-card__title">${productName}</span>
-                        <span class="product-card__price">${productPrice} ₽</span>
-                    </div>
-                </a>
-            `;
-            cardsContainer.insertAdjacentHTML("beforeend", productCardEl);
-        });
+async function getData() {
+    const response = await fetch("js/data.json");
+    const data = await response.json();
+    return data;
+}
+
+async function addProductCard() {
+    let postsData = await getData();
+    const cardsContainer = document.querySelector(".products__main");
+
+    postsData = postsData.reverse().slice(0, 8);
+    postsData.forEach(({ productId, imgSrc, productName, productPrice }) => {
+        const productCardEl = `
+            <a id="${productId}" href="#" class="product-card">
+                <img src="${imgSrc}" alt="Product photo" class="product-card__img">
+                <div class="product-card__text">
+                    <span class="product-card__title">${productName}</span>
+                    <span class="product-card__price">${productPrice} ₽</span>
+                </div>
+            </a>
+        `;
+        cardsContainer.insertAdjacentHTML("beforeend", productCardEl);
     });
+}
+
+addProductCard();
