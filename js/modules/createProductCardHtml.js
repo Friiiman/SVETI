d55b8e41a8992c8275cf6a9bd87fbaf65395e753
Максим Савчук <friiiman94@gmail.com>
2025-01-14 22:08:05 +0300

    up-button add

--- js/modules/createProductCardHtml.js
+++ js/modules/createProductCardHtml.js
@@ -2,8 +2,8 @@ export function createProductCardHtml(postsData, cardsContainer) {
     postsData.forEach(({ productId, imgSrc, additionalImgSrcFirst, productName, productPrice }) => {
         const productCardEl = `
             <article id="${productId}" class="product-card">
-                <img src="${imgSrc}" alt="Product photo" class="product-card__img">
-                <img src="${additionalImgSrcFirst}" alt="Product photo" class="product-card__img product-card__img-hover">
+                <img src="${imgSrc}" alt="Product photo" loading="lazy" class="product-card__img">
+                <img src="${additionalImgSrcFirst}" alt="Product photo" loading="lazy" class="product-card__img product-card__img-hover">
                 <div class="product-card__text">
                     <h3 class="product-card__title">${productName}</h3>
                     <span class="product-card__price">${productPrice} ₽</span>
---
+++ js/modules/upButton.js
@@ -0,0 +1,15 @@
+export function upButton() {
+    
+    document.addEventListener("DOMContentLoaded", function () {
+        const backToTop = document.querySelector(".up-button");
+        const backToTopHidden = "up-button--hidden";
+
+        window.addEventListener("scroll", function () {
+            if (window.scrollY >= 300) {
+                backToTop.classList.remove(backToTopHidden);
+            } else {
+                backToTop.classList.add(backToTopHidden);
+            }
+        });
+    });
+}
